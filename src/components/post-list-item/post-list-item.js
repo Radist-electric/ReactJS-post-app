57256d9c977d7db3c9c3615ee328fd8c90fe0a34
Radist-electric <radist-electric@yandex.ru>
2019-01-22 12:29:33 +0500

    Lesson-03 difficult

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
+let currentDate = new Date(),
+    Year = currentDate.getFullYear(),
+    Month = currentDate.getMonth(),
+    Day = currentDate.getDate(),
+    Hour = currentDate.getHours(),
+    Minutes = currentDate.getMinutes(),
+    fMonth = '';
+    switch (Month)
+    {
+      case 0: fMonth="января"; break;
+      case 1: fMonth="февраля"; break;
+      case 2: fMonth="марта"; break;
+      case 3: fMonth="апреля"; break;
+      case 4: fMonth="мае"; break;
+      case 5: fMonth="июня"; break;
+      case 6: fMonth="июля"; break;
+      case 7: fMonth="августа"; break;
+      case 8: fMonth="сентября"; break;
+      case 9: fMonth="октября"; break;
+      case 10: fMonth="ноября"; break;
+      case 11: fMonth="декабря"; break;
+      default:
+    };
+
 const PostListItem = () => {
     return (
         <li className="app-list-item d-flex justify-content-between">
             <span className="app-list-item-label">
                 Hello World!!!
+                <p className="app-list-item-date">Дата публикации: {Day}-{fMonth}-{Year} {Hour}:{Minutes}</p>
             </span>
             <div className="d-flex justify-content-center align-items-center">
                 <button
@@ -24,3 +49,4 @@ const PostListItem = () => {
 }
 
 export default PostListItem;
+
